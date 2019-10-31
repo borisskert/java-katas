6f73e19724a5d76e82d47c3aed3269f3baaa6d47
Boris Skert <boris.skert@adorsys.de>
2019-10-31 16:54:07 +0100

    clean up

--- src/main/java/interlacedspiralcipher/InterlacedSpiralCipher.java
+++ src/main/java/interlacedspiralcipher/InterlacedSpiralCipher.java
@@ -6,20 +6,25 @@ import java.util.Map;
 public class InterlacedSpiralCipher {
 
     public static String encode(String s) {
-        TwoWayHashMap<Integer, Integer> map = createMap(s.length());
+        TwoWayHashMap<Integer, Integer> map = createSpiralCipherMap(s.length());
 
         char[] result = new char[map.size()];
 
-        for (int index = 0; index < s.length(); index++) {
+        for (int index = 0; index < map.size(); index++) {
             Integer encodeIndex = map.getForward(index);
-            result[index] = s.charAt(encodeIndex);
+
+            if(encodeIndex >= s.length()) {
+                result[index] = ' ';
+            } else {
+                result[index] = s.charAt(encodeIndex);
+            }
         }
 
         return String.valueOf(result);
     }
 
     public static String decode(String s) {
-        TwoWayHashMap<Integer, Integer> map = createMap(s.length());
+        TwoWayHashMap<Integer, Integer> map = createSpiralCipherMap(s.length());
 
         char[] result = new char[map.size()];
 
@@ -28,41 +33,35 @@ public class InterlacedSpiralCipher {
             result[index] = s.charAt(decodeIndex);
         }
 
-        return String.valueOf(result);
+        return String.valueOf(result).trim();
     }
 
-    private static TwoWayHashMap<Integer, Integer> createMap(int textLength) {
+    private static TwoWayHashMap<Integer, Integer> createSpiralCipherMap(int textLength) {
         int cipherLength = length(textLength);
         int[] spiral = new int[cipherLength];
 
-        int ring = (int) Math.min(1, Math.sqrt(cipherLength) - 2);
         int round = 0;
         int offset = 0;
         int step = 0;
         int index = 0;
-        int size = 2 * ring + 1;
+        int size = (int)Math.sqrt(cipherLength) - 1;
 
-        for (; index < textLength; index++) {
+        for (; index < cipherLength; index++) {
             if (round >= 4) {
                 round = 0;
                 step++;
             }
 
             if (step >= size) {
-                offset = size * 4;
-                ring--;
+                offset += size * 4;
                 step = 0;
-                size = 2 * ring + 1;
+                size -= 2;
             }
 
             int position = offset + step + size * round;
             spiral[position] = index;
 
             round++;
-        }
-
-        for (; index < spiral.length; index++) {
-            spiral[index] = 0;
         }
 
         Snail snail = new Snail(textLength);
@@ -88,8 +87,6 @@ public class InterlacedSpiralCipher {
         private final T[][] values;
         private Direction direction = Direction.E;
 
-        private final int size;
-
         private int maxX;
         private int maxY;
         private int minX;
@@ -98,15 +95,15 @@ public class InterlacedSpiralCipher {
         private int x = 0;
         private int y = 0;
 
-        private Snail(int size) {
-            this.size = length(size);
-            this.values = create(size);
+        private Snail(int textSize) {
+            int size = snailSize(textSize);
+            this.values = create(textSize);
 
             this.minX = -1;
             this.minY = -1;
 
-            this.maxX = this.size;
-            this.maxY = this.size;
+            this.maxX = size;
+            this.maxY = size;
         }
 
         public void setNext(T c) {
@@ -171,7 +168,7 @@ public class InterlacedSpiralCipher {
         }
 
         private T[][] create(int size) {
-            int length = length(size);
+            int length = snailSize(size);
             T[][] values = (T[][]) new Object[length][];
 
             for (int i = 0; i < length; i++) {
@@ -181,7 +178,7 @@ public class InterlacedSpiralCipher {
             return values;
         }
 
-        private static int length(int textLength) {
+        private static int snailSize(int textLength) {
             int squareRoot = (int) Math.sqrt(textLength);
             int power = squareRoot * squareRoot;
 
--- src/test/java/interlacedspiralcipher/InterlacedSpiralCipherTest.java
+++ src/test/java/interlacedspiralcipher/InterlacedSpiralCipherTest.java
@@ -7,14 +7,16 @@ import static org.junit.Assert.*;
 public class InterlacedSpiralCipherTest {
     @Test
     public void SampleTests() {
-
         String clear = "Romani ite domum",
                 crypt = "Rntodomiimuea  m";
         assertEquals(crypt, InterlacedSpiralCipher.encode(clear));
         assertEquals(clear, InterlacedSpiralCipher.decode(crypt));
+    }
 
-        clear = "Sic transit gloria mundi";
-        crypt = "Stsgiriuar i ninmd l otac";
+    @Test
+    public void SampleTests2() {
+        String clear = "Sic transit gloria mundi";
+        String crypt = "Stsgiriuar i ninmd l otac";
         assertEquals(crypt, InterlacedSpiralCipher.encode(clear));
         assertEquals(clear, InterlacedSpiralCipher.decode(crypt));
     }
