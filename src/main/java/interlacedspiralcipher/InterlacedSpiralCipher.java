1c9d69575b19d372cd355405c0cce01e18d0392c
Boris Skert <boris.skert@adorsys.de>
2019-10-31 14:13:04 +0100

    reverse spiral cipher

--- src/main/java/interlacedspiralcipher/InterlacedSpiralCipher.java
+++ src/main/java/interlacedspiralcipher/InterlacedSpiralCipher.java
@@ -6,36 +6,49 @@ import java.util.Map;
 public class InterlacedSpiralCipher {
 
     public static String encode(String s) {
-        Map<Integer, Integer> map = createMap(s.length());
+        TwoWayHashMap<Integer, Integer> map = createMap(s.length());
 
         char[] result = new char[map.size()];
 
-        for(int index = 0; index < s.length(); index++) {
-            Integer cipherIndex = map.get(index);
-            result[index] = s.charAt(cipherIndex);
+        for (int index = 0; index < s.length(); index++) {
+            Integer encodeIndex = map.getForward(index);
+            result[index] = s.charAt(encodeIndex);
         }
 
         return String.valueOf(result);
     }
 
-    private static Map<Integer, Integer> createMap(int textLength) {
+    public static String decode(String s) {
+        TwoWayHashMap<Integer, Integer> map = createMap(s.length());
+
+        char[] result = new char[map.size()];
+
+        for (int index = 0; index < s.length(); index++) {
+            Integer decodeIndex = map.getBackward(index);
+            result[index] = s.charAt(decodeIndex);
+        }
+
+        return String.valueOf(result);
+    }
+
+    private static TwoWayHashMap<Integer, Integer> createMap(int textLength) {
         int cipherLength = length(textLength);
         int[] spiral = new int[cipherLength];
 
-        int ring = (int)Math.min(1, Math.sqrt(cipherLength) - 2);
+        int ring = (int) Math.min(1, Math.sqrt(cipherLength) - 2);
         int round = 0;
         int offset = 0;
         int step = 0;
         int index = 0;
         int size = 2 * ring + 1;
 
-        for(; index < textLength; index++) {
-            if(round >= 4) {
+        for (; index < textLength; index++) {
+            if (round >= 4) {
                 round = 0;
                 step++;
             }
 
-            if(step >= size) {
+            if (step >= size) {
                 offset = size * 4;
                 ring--;
                 step = 0;
@@ -48,28 +61,23 @@ public class InterlacedSpiralCipher {
             round++;
         }
 
-        for(; index < spiral.length; index++) {
-            spiral[index] = ' ';
+        for (; index < spiral.length; index++) {
+            spiral[index] = 0;
         }
 
         Snail snail = new Snail(textLength);
-        for(index = 0; index < spiral.length; index++) {
+        for (index = 0; index < spiral.length; index++) {
             snail.setNext(spiral[index]);
         }
 
         return snail.getMap();
     }
 
-    public static String decode(String s) {
-        // Your code here!
-        return "";
-    }
-
     private static int length(int textLength) {
-        int squareRoot = (int)Math.sqrt(textLength);
+        int squareRoot = (int) Math.sqrt(textLength);
         int power = squareRoot * squareRoot;
 
-        if(power < textLength) {
+        if (power < textLength) {
             return (squareRoot + 1) * (squareRoot + 1);
         }
 
@@ -164,41 +172,64 @@ public class InterlacedSpiralCipher {
 
         private T[][] create(int size) {
             int length = length(size);
-            T[][] values = (T[][])new Object[length][];
+            T[][] values = (T[][]) new Object[length][];
 
-            for(int i = 0; i < length; i++) {
-                values[i] = (T[])new Object[length];
+            for (int i = 0; i < length; i++) {
+                values[i] = (T[]) new Object[length];
             }
 
             return values;
         }
 
         private static int length(int textLength) {
-            int squareRoot = (int)Math.sqrt(textLength);
+            int squareRoot = (int) Math.sqrt(textLength);
             int power = squareRoot * squareRoot;
 
-            if(power < textLength) {
+            if (power < textLength) {
                 return squareRoot + 1;
             }
 
             return squareRoot;
         }
 
-        public Map<Integer, T> getMap() {
-            Map<Integer, T> map = new HashMap<>();
+        public TwoWayHashMap<Integer, T> getMap() {
+            TwoWayHashMap<Integer, T> map = new TwoWayHashMap<>();
 
             int index = 0;
-            for(int x = 0; x < values.length; x++) {
-                for(int y = 0; y < values[x].length; y++) {
+            for (int x = 0; x < values.length; x++) {
+                for (int y = 0; y < values[x].length; y++) {
 
                     T object = values[x][y];
-                    map.put(index, object);
+                    map.add(index, object);
 
                     index++;
                 }
             }
 
             return map;
+        }
+    }
+
+    public static class TwoWayHashMap<K extends Object, V extends Object> {
+
+        private Map<K, V> forward = new HashMap<>();
+        private Map<V, K> backward = new HashMap<>();
+
+        public synchronized void add(K key, V value) {
+            forward.put(key, value);
+            backward.put(value, key);
+        }
+
+        public synchronized V getForward(K key) {
+            return forward.get(key);
+        }
+
+        public synchronized K getBackward(V key) {
+            return backward.get(key);
+        }
+
+        public int size() {
+            return forward.size();
         }
     }
 }
