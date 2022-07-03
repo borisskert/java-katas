e9ed89d57d93946d56a4f4d6513da66d4fa647a5
Boris Skert <boris.skert@gmail.com>
2022-07-03 09:51:31 +0200

    Setup kata

---
+++ src/main/java/totalareacoveredbyrectangles/RectanglesUnion.java
@@ -0,0 +1,10 @@
+package totalareacoveredbyrectangles;
+
+/**
+ * <a href="https://www.codewars.com/kata/55dcdd2c5a73bdddcb000044/train/java">Total area covered by rectangles</a>
+ */
+public class RectanglesUnion {
+    public static int calculateSpace(int[][] rectangles) {
+        return 0;
+    }
+}
---
+++ src/test/java/totalareacoveredbyrectangles/RectanglesUnionTest.java
@@ -0,0 +1,18 @@
+package totalareacoveredbyrectangles;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RectanglesUnionTest {
+    @Test
+    public void testZeroRectangles() {
+        int[][] recs = {};
+        Assert.assertEquals("Zero rectangles", 0, RectanglesUnion.calculateSpace(recs));
+    }
+
+    @Test
+    public void testOneRectangle() {
+        int[][] recs = {{0, 4, 11, 6}};
+        Assert.assertEquals("One rectangle [0, 4, 11, 6] => 22", 22, RectanglesUnion.calculateSpace(recs));
+    }
+}
